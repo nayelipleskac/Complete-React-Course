607197976c9012a4f176397a01446837e772cf92
Nayeli Pleskac <pleskac510@gmail.com>
2022-01-10 19:43:23 -0800

    01-starting code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+ReactDOM.render(<App />, document.getElementById('root'));
