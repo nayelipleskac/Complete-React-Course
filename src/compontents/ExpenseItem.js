09633a4e03790194c9b2cc9fa68576c2c567753f
Nayeli Pleskac <pleskac510@gmail.com>
2022-01-22 22:59:21 -0800

    jsx

--- src/compontents/ExpenseItem.js
+++ src/compontents/ExpenseItem.js
@@ -3,16 +3,12 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2022, 1, 15);
-    const expenseTitle = 'Books';
-    const expenseAmount = 29.99;
-
     return (
         <div className ="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className = "expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className = "expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className = "expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
