517ef13b9a12f33abe3f7f9fa45cda18cdd9ee1d
Nayeli Pleskac <pleskac510@gmail.com>
2022-02-21 15:04:01 -0800

    expenses.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //npx create-react-app: https://create-react-app.dev/docs/getting-started
 
-import ExpenseItem from './compontents/ExpenseItem'
+import Expenses from './compontents/Expenses';
 
 function App() {
 
@@ -29,10 +29,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <Expenses items = {expenses}/>
 
     </div>
   );
